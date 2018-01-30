1389f80777bfcdc9132330ada3e2ab667c4080b2
jorgeMunozMartinez <32263233+jorgeMunozMartinez@users.noreply.github.com>
2018-01-30 20:24:50 +0100

    cambio completo al programa

---
+++ C_02/src/Persistencia/DistribucionTerreno.java
@@ -0,0 +1,55 @@
+package Persistencia;
+/**
+ * 
+ * Clase se encraga de decir cual posicion y cuanta tierra se movera
+ *  
+ * @author Jorge
+ * 
+ * @version 1.0
+ *
+ */
+
+public class DistribucionTerreno {
+	
+	private int posX;
+	private int posY;
+	private int arena;
+	
+	public DistribucionTerreno(int posX, int posY, int arena) {
+		this.posX = posX;
+		this.posY = posY;
+		this.arena = arena;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getArena() {
+		return arena;
+	}
+
+	public void setArena(int arena) {
+		this.arena = arena;
+	}
+
+	@Override
+	public String toString() {
+		return "[posX=" + posX + ", posY=" + posY + ", arena=" + arena + "]";
+	}
+	
+	
+
+}
